ef2dc09b3c03e18ea113d9f03fae8ffdb16c9eb8
José Borges Ferreira <underspell@gmail.com>
2017-01-26 20:44:28 +0000

    bashcov fullpath

--- misc/ci-runtest.sh
+++ misc/ci-runtest.sh
@@ -17,7 +17,7 @@ host jasan.tk || true
 
 echo "COVERAGE is $COVERAGE"
 if [ "x1" = "x$COVERAGE" ] ; then
-	bashcov -- tests/test-shell.sh
+	/home/travis/.rvm/gems/ruby-2.2.5/wrappers/bashcov -- tests/test-shell.sh
 else
-	bashcov -- ./despf.sh borgesferreira.pt
+	tests/test-shell.sh
 fi
